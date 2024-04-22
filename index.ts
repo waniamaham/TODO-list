5f6811f64933f3c1d4eb5c00e47baa021cf738c0
waniamaham <M.ShakirAli786@outlook.com>
2024-04-22 06:47:25 +0500

    The body provides details about the specific changes made, such as adding features to mark tasks as completed and delete tasks, as well as improvements in error handling and code refactoring.

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos = [];
+let condition = true;
+console.log(chalk.blue.bold("\n \t WELLCOME TO MY TODO LIST APPLICATION! \n"));
+
+while(condition)
+    {
+        let addTask =await inquirer.prompt(
+        [
+            {
+                name:"todo",
+                type:"input",
+                message:chalk.yellowBright("What you want to add in your todos?")
+            },
+            
+            {
+                name:"addMore",
+                type:"confirm",
+                message:chalk.yellowBright("Are you sure! Do you want to add more?"),
+                default:"false"
+            }
+        ]
+    );
+    
+    todos.push(addTask.todo);
+    console.log(chalk.magenta.bold("Item is add Succesfully in your TO-DO List (^_^)"))
+    condition = addTask.addMore
+    console.log(todos);
+    
+    
+}
